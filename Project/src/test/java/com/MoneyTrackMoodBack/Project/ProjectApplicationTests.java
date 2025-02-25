cb4b1b99e83bf5d875776582ea80c112cce2ffe5
Farah <farahchaouch73@gmail.com>
2025-02-25 13:45:00 +0100

    Initial commit

---
+++ Project/src/test/java/com/MoneyTrackMoodBack/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoneyTrackMoodBack.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
